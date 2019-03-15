38c7c58b6c9fced9ab8c7f76f7e0f51a6a07a315
rnm-patel <rajatmovaliya512@gmail.com>
2019-03-15 22:03:13 +0530

    upgrading version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('aider/requirements.txt') as fp:
 setup(
     name = 'aider',         
     packages = ['aider'],
-    version = '0.1',      
+    version = '0.2',      
     license='MIT',        
     description = 'general utilities',     
     long_description = open('README.rst').read(),
